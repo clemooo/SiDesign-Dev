6fd19c071819df7cf9952743e8b8ac9d6ddeeb0f
fendorei <40180897+fendorei@users.noreply.github.com>
2019-10-10 23:46:33 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 const stones = document.querySelectorAll("#stone")
+const gauntlet = document.querySelector(".gauntlet")
 const gems = document.querySelectorAll(".gem")
 const story = document.querySelectorAll("#story")
 const container = document.querySelector(".container-gant")
 const origin = document.querySelector(".fond")
 const background = document.querySelectorAll(".atmo")
+const video = document.querySelector("video")
 
 stones[0].onclick = function(){take_stone(stones, gems, story, background, 0)}
 stones[1].onclick = function(){take_stone(stones, gems, story, background, 1)}
@@ -18,12 +20,23 @@ function take_stone(stones, gems, story, background, type){
     origin.style.display = "none"
     background[type].style.display = "initial"
     for (i = 0; i <= 5; i++) {
-	if (i != type)
-	    background[i].style.display = "none"
-	story[i].pause()
+        if (i != type)
+            background[i].style.display = "none"
+        story[i].pause()
     }
     stones[type].style.display = "none"
     gems[type].style.display = "initial"
     story[type].play()
+    for (i = 0; i <= 5 && gems[i].style.display == "initial"; i++) {}
+    if (i == 6) {
+        setTimeout(function() {
+            video.style.display = "initial"
+            gauntlet.style.display = "none"
+            for (i = 0; i <= 5; i++)
+                gems[i].style.display = "none"
+            video.play()
+        }, story[type].duration * 1000);
+    }
 }
+
 
