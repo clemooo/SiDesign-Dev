19b563014ce52f57c5d28a35108611be3673730d
Dimitri Le Net <dimitri.lenet@hetic.net>
2019-10-11 00:15:00 +0200

    commit les droits

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
-const stones = document.querySelectorAll("#stone")
-const gauntlet = document.querySelector(".gauntlet")
-const gems = document.querySelectorAll(".gem")
-const story = document.querySelectorAll("#story")
-const container = document.querySelector(".container-gant")
-const origin = document.querySelector(".fond")
-const background = document.querySelectorAll(".atmo")
-const video = document.querySelector("video")
+const stones = document.querySelectorAll("#stone");
+const gauntlet = document.querySelector(".gauntlet");
+const gems = document.querySelectorAll(".gem");
+const story = document.querySelectorAll("#story");
+const container = document.querySelector(".container-gant");
+const origin = document.querySelector(".fond");
+const background = document.querySelectorAll(".atmo");
+const video = document.querySelector("video");
 
 stones[0].onclick = function() {
   take_stone(stones, gems, story, background, 0);
@@ -26,45 +26,40 @@ stones[5].onclick = function() {
   take_stone(stones, gems, story, background, 5);
 };
 
-function take_stone(stones, gems, story, background, type) {
-  let i = 0;
+  //music off
+  const musicButton = document.querySelector(".intro-music");
+  const musicAudio = document.querySelector(".intro");
+  const OffOn2 = document.querySelector(".musicOffOn");
+  let switch2 = false;
 
-//music off 
-const musicButton = document.querySelector(".intro-music");
-const musicAudio = document.querySelector(".intro");
-const OffOn2 = document.querySelector(".musicOffOn");
-let switch2 = false;
-
-musicButton.addEventListener("click", function() {
-  if (switch2 == false) {
-    musicAudio.pause();
-    OffOn2.innerHTML = "Off";
-  } else {
-    musicAudio.play();
-    OffOn2.innerHTML = "On";
+  musicButton.addEventListener("click", function() {
+    console.log("click");
+    if (switch2 == false) {
+      musicAudio.pause();
+      OffOn2.innerHTML = "Off";
+    } else {
+      musicAudio.play();
+      OffOn2.innerHTML = "On";
+    }
+    switch2 = !switch2;
+  });
+  //animation du gant
+  origin.style.display = "none";
+  background[type].style.display = "initial";
+  for (i = 0; i <= 5; i++) {
+    if (i != type) background[i].style.display = "none";
+    story[i].pause();
   }
-  switch2 = !switch2;
-});
-    origin.style.display = "none"
-    background[type].style.display = "initial"
-    for (i = 0; i <= 5; i++) {
-        if (i != type)
-            background[i].style.display = "none"
-        story[i].pause()
-    }
-    stones[type].style.display = "none"
-    gems[type].style.display = "initial"
-    story[type].play()
-    for (i = 0; i <= 5 && gems[i].style.display == "initial"; i++) {}
-    if (i == 6) {
-        setTimeout(function() {
-            video.style.display = "initial"
-            gauntlet.style.display = "none"
-            for (i = 0; i <= 5; i++)
-                gems[i].style.display = "none"
-            video.play()
-        }, story[type].duration * 1000);
-    }
+  stones[type].style.display = "none";
+  gems[type].style.display = "initial";
+  story[type].play();
+  for (i = 0; i <= 5 && gems[i].style.display == "initial"; i++) {}
+  if (i == 6) {
+    setTimeout(function() {
+      video.style.display = "initial";
+      gauntlet.style.display = "none";
+      for (i = 0; i <= 5; i++) gems[i].style.display = "none";
+      video.play();
+    }, story[type].duration * 1000);
+  }
 }
-
-
