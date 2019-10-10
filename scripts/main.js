cf39b651c563e3c00bcadbd6efb38e906e468e46
fendorei <40180897+fendorei@users.noreply.github.com>
2019-10-10 09:12:12 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,19 @@ const stones = document.querySelectorAll("#stone")
 const gems = document.querySelectorAll(".gem")
 const story = document.querySelectorAll("#story")
 
-stones[0].onclick = function(){take_stone(stones[0], gems[0], story[0])}
-stones[1].onclick = function(){take_stone(stones[1], gems[1], story[1])}
-stones[2].onclick = function(){take_stone(stones[2], gems[2], story[2])}
-stones[3].onclick = function(){take_stone(stones[3], gems[3], story[3])}
-stones[4].onclick = function(){take_stone(stones[4], gems[4], story[4])}
-stones[5].onclick = function(){take_stone(stones[5], gems[5], story[5])}
+stones[0].onclick = function(){take_stone(stones, gems, story, 0)}
+stones[1].onclick = function(){take_stone(stones, gems, story, 1)}
+stones[2].onclick = function(){take_stone(stones, gems, story, 2)}
+stones[3].onclick = function(){take_stone(stones, gems, story, 3)}
+stones[4].onclick = function(){take_stone(stones, gems, story, 4)}
+stones[5].onclick = function(){take_stone(stones, gems, story, 5)}
 
-function take_stone(stone, gem, story){
-    stone.style.display = "none"
-    gem.style.display = "initial"
-    story.play()
+function take_stone(stones, gems, story, type){
+    stones[type].style.display = "none"
+    gems[type].style.display = "initial"
+    story[type].play()
+    for (let i = 0; i < 6 || gems[i].style.display == "initial"; i++) {}
+    if (i == 6)
+	story[6].play()
 }
 
