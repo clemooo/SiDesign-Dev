987178120dcffeaf10fd01ff87b798a7938091d7
Dimitri Le Net <dimitri.lenet@hetic.net>
2019-10-11 00:24:27 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,9 @@
 const stones = document.querySelectorAll("#stone");
-const gauntlet = document.querySelector(".gauntlet");
 const gems = document.querySelectorAll(".gem");
 const story = document.querySelectorAll("#story");
 const container = document.querySelector(".container-gant");
 const origin = document.querySelector(".fond");
 const background = document.querySelectorAll(".atmo");
-const video = document.querySelector("video");
 
 stones[0].onclick = function() {
   take_stone(stones, gems, story, background, 0);
@@ -26,24 +24,9 @@ stones[5].onclick = function() {
   take_stone(stones, gems, story, background, 5);
 };
 
-  //music off
-  const musicButton = document.querySelector(".intro-music");
-  const musicAudio = document.querySelector(".intro");
-  const OffOn2 = document.querySelector(".musicOffOn");
-  let switch2 = false;
+function take_stone(stones, gems, story, background, type) {
+  let i = 0;
 
-  musicButton.addEventListener("click", function() {
-    console.log("click");
-    if (switch2 == false) {
-      musicAudio.pause();
-      OffOn2.innerHTML = "Off";
-    } else {
-      musicAudio.play();
-      OffOn2.innerHTML = "On";
-    }
-    switch2 = !switch2;
-  });
-  //animation du gant
   origin.style.display = "none";
   background[type].style.display = "initial";
   for (i = 0; i <= 5; i++) {
@@ -53,13 +36,21 @@ stones[5].onclick = function() {
   stones[type].style.display = "none";
   gems[type].style.display = "initial";
   story[type].play();
-  for (i = 0; i <= 5 && gems[i].style.display == "initial"; i++) {}
-  if (i == 6) {
-    setTimeout(function() {
-      video.style.display = "initial";
-      gauntlet.style.display = "none";
-      for (i = 0; i <= 5; i++) gems[i].style.display = "none";
-      video.play();
-    }, story[type].duration * 1000);
-  }
 }
+
+//music off 
+const musicButton = document.querySelector(".intro-music");
+const musicAudio = document.querySelector(".intro");
+const OffOn2 = document.querySelector(".musicOffOn");
+let switch2 = false;
+
+musicButton.addEventListener("click", function() {
+  if (switch2 == false) {
+    musicAudio.pause();
+    OffOn2.innerHTML = "Off";
+  } else {
+    musicAudio.play();
+    OffOn2.innerHTML = "On";
+  }
+  switch2 = !switch2;
+});
