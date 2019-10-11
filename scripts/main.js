0fef275a70884fef17c15420d4e037b18bae8571
Clémo <clemo.daurios@gmail.com>
2019-10-11 10:29:31 +0200

    fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,43 @@
-const stones = document.querySelectorAll("#stone");
-const gems = document.querySelectorAll(".gem");
-const story = document.querySelectorAll("#story");
-const container = document.querySelector(".container-gant");
-const origin = document.querySelector(".fond");
-const background = document.querySelectorAll(".atmo");
+@@ -1,9 +1,11 @@
+const stones = document.querySelectorAll("#stone")
+const gauntlet = document.querySelector(".gauntlet")
+const gems = document.querySelectorAll(".gem")
+const story = document.querySelectorAll("#story")
+const container = document.querySelector(".container-gant")
+const origin = document.querySelector(".fond")
+const background = document.querySelectorAll(".atmo")
+const video = document.querySelector("video")
 
-stones[0].onclick = function() {
-  take_stone(stones, gems, story, background, 0);
-};
-stones[1].onclick = function() {
-  take_stone(stones, gems, story, background, 1);
-};
-stones[2].onclick = function() {
-  take_stone(stones, gems, story, background, 2);
-};
-stones[3].onclick = function() {
-  take_stone(stones, gems, story, background, 3);
-};
-stones[4].onclick = function() {
-  take_stone(stones, gems, story, background, 4);
-};
-stones[5].onclick = function() {
-  take_stone(stones, gems, story, background, 5);
-};
-
-function take_stone(stones, gems, story, background, type) {
-  let i = 0;
-
-  origin.style.display = "none";
-  background[type].style.display = "initial";
-  for (i = 0; i <= 5; i++) {
-    if (i != type) background[i].style.display = "none";
-    story[i].pause();
-  }
-  stones[type].style.display = "none";
-  gems[type].style.display = "initial";
-  story[type].play();
+stones[0].onclick = function(){take_stone(stones, gems, story, background, 0)}
+stones[1].onclick = function(){take_stone(stones, gems, story, background, 1)}
+@ -18,12 +20,23 @@ function take_stone(stones, gems, story, background, type){
+    origin.style.display = "none"
+    background[type].style.display = "initial"
+    for (i = 0; i <= 5; i++) {
+	if (i != type)
+	    background[i].style.display = "none"
+	story[i].pause()
+        if (i != type)
+            background[i].style.display = "none"
+        story[i].pause()
+    }
+    stones[type].style.display = "none"
+    gems[type].style.display = "initial"
+    story[type].play()
+    for (i = 0; i <= 5 && gems[i].style.display == "initial"; i++) {}
+    if (i == 6) {
+        setTimeout(function() {
+            video.style.display = "initial"
+            gauntlet.style.display = "none"
+            for (i = 0; i <= 5; i++)
+                gems[i].style.display = "none"
+            video.play()
+        }, story[type].duration * 1000);
+    }
 }
 
-//music off 
+
+//music off
 const musicButton = document.querySelector(".intro-music");
 const musicAudio = document.querySelector(".intro");
 const OffOn2 = document.querySelector(".musicOffOn");
